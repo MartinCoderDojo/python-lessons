0386328bb97033bcd0cf5ab8d40c5160684610e3
Martin <martin@coderdojoscotland.com>
2018-02-03 20:54:53 +0000

    Changed structure of maze
    Flake 8 compliant

--- Lesson2/mazeSimple3.py
+++ Lesson2/mazeSimple3.py
@@ -5,10 +5,6 @@ def moveforward():
     arrow.goto(arrow.position()[0]+45, arrow.position()[1])
 
 
-def moveup():
-    arrow.goto(arrow.position()[0], arrow.position()[1]+55)
-
-
 def complete():
     message = turtle.Turtle()
     message.penup()
@@ -22,43 +18,21 @@ def complete():
 
 wall = turtle.Turtle()
 wall.penup()
-wall.goto(-150, -150)
+wall.goto(-150, 0)
 wall.pendown()
-wall.forward(115)
-wall.left(90)
+wall.forward(300)
+wall.right(90)
 wall.forward(50)
 wall.right(90)
-
-for x in range(2):
-    wall.forward(90)
-    wall.left(90)
-    wall.forward(50)
-    wall.right(90)
-
-wall.left(90)
-wall.forward(50)
-wall.left(90)
-wall.forward(50)
-wall.left(90)
-wall.forward(50)
-
-for x in range(2):
-    wall.right(90)
-    wall.forward(90)
-    wall.left(90)
-    wall.forward(50)
-
+wall.forward(300)
 wall.right(90)
-wall.forward(65)
-wall.left(90)
 wall.forward(50)
-
 wall.penup()
 wall.hideturtle()
 
 arrow = turtle.Turtle()
 arrow.penup()
-arrow.goto(-135, -125)
+arrow.goto(-135, -25)
 arrow.left(90)
 
 # Insert your code after this line
--- Lesson2/mazeSimple4.py
+++ Lesson2/mazeSimple4.py
@@ -2,19 +2,19 @@ import turtle
 
 
 def moveforward():
-    arrow.goto(arrow.position()[0]+45, arrow.position()[1])
+    arrow.goto(arrow.position()[0]+25, arrow.position()[1])
 
 
 def moveup():
-    arrow.goto(arrow.position()[0], arrow.position()[1]+55)
+    arrow.goto(arrow.position()[0], arrow.position()[1]+50)
 
 
 def complete():
     message = turtle.Turtle()
     message.penup()
     message.hideturtle()
-    message.goto(-40, 100)
-    if(arrow.position()[0] == 135):
+    message.goto(-40, 120)
+    if(arrow.position()[0] == 90):
         message.write('Success', font=('Arial', 18, 'normal'))
     else:
         message.write('Try Again', font=('Arial', 18, 'normal'))
@@ -22,15 +22,16 @@ def complete():
 
 wall = turtle.Turtle()
 wall.penup()
-wall.goto(-150, -150)
+wall.goto(-125, -150)
 wall.pendown()
-wall.forward(115)
+
+wall.forward(100)
 wall.left(90)
 wall.forward(50)
 wall.right(90)
 
-for x in range(2):
-    wall.forward(90)
+for x in range(3):
+    wall.forward(50)
     wall.left(90)
     wall.forward(50)
     wall.right(90)
@@ -42,23 +43,19 @@ wall.forward(50)
 wall.left(90)
 wall.forward(50)
 
-for x in range(2):
+for x in range(4):
     wall.right(90)
-    wall.forward(90)
+    wall.forward(50)
     wall.left(90)
     wall.forward(50)
-
-wall.right(90)
-wall.forward(65)
-wall.left(90)
-wall.forward(50)
 
 wall.penup()
 wall.hideturtle()
 
 arrow = turtle.Turtle()
 arrow.penup()
-arrow.goto(-135, -125)
+
+arrow.goto(-110, -125)
 arrow.left(90)
 
 # Insert your code after this line
